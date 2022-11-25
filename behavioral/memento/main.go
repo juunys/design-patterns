cc295857ac6d721bf984f6bc57fe322b820464ec
Jun Yamada <jun3120@gmail.com>
2022-11-24 23:30:06 -0300

    feat: memento

---
+++ behavioral/memento/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	caretaker := NewCaretaker()
+	originator := NewOriginator("A")
+
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.SetState("B")
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.SetState("C")
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.RestoreMemento(caretaker.GetMemento(1))
+	fmt.Printf("Restored to State: %s\n", originator.GetState())
+
+	originator.RestoreMemento(caretaker.GetMemento(0))
+	fmt.Printf("Restored to State: %s\n", originator.GetState())
+
+}
