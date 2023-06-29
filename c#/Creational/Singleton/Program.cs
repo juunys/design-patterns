8b5fb936a706362f1ebef28780a6a720013b38db
Jun Yamada <jun3120@gmail.com>
2023-06-29 14:36:37 -0300

    feat: c# singleton

---
+++ c#/Creational/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => ThreadAccess1(),
+                () => ThreadAccess2()
+            );
+            Console.ReadLine();
+        }
+
+        private static void ThreadAccess1()
+        {
+            Singleton s1 = Singleton.Instance;
+            Console.WriteLine("Thread 1");
+        }
+
+        private static void ThreadAccess2()
+        {
+            Singleton s2 = Singleton.Instance;
+            Console.WriteLine("Thread 2");
+        }
+    }
+}
---
+++ c#/Creational/Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Singleton
+{
+	public class Singleton
+	{
+		private static Singleton instance = null;
+		private int instanceNumber = 0;
+		private static object lockObject = new object();
+
+		private Singleton()
+		{
+			Console.WriteLine("Instantiating inside private constructor");
+			instanceNumber++;
+			Console.WriteLine($"Instance number: {instanceNumber} \n");
+		}
+
+		public static Singleton Instance
+		{
+			get
+			{
+				lock(lockObject)
+				{
+                    if (instance == null)
+						instance = new Singleton();
+                }
+				return instance;
+			}
+		}
+	}
+}
+
